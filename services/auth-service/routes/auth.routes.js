3556ac14b5470d2dc6f75c15f6efb92e4dee64c6
Vijay Rajkumar More <vijaymore6212@gmail.com>
2026-06-21 22:48:01 +0530

    Added admin role in auth.routes.js

--- services/auth-service/routes/auth.routes.js
+++ services/auth-service/routes/auth.routes.js
@@ -29,7 +29,17 @@ router.post('/register', async (req, res) => {
     if (existing) return res.status(400).json({ message: 'Email already registered' });
 
     const hashed = await bcrypt.hash(password, 10);
-    const user = await User.create({ name, email, password: hashed });
+    const role =
+    email === process.env.ADMIN_EMAIL
+      ? "admin"
+      : "customer";
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashed,
+    role
+  });
 
     const { accessToken, refreshToken } = generateTokens(user);
     user.refreshToken = refreshToken;
